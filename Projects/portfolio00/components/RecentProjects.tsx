7b36d1070a73e66b7284cf5ebb4be12ca923d279
ECardona97 <ecardona742@gmail.com>
2024-09-30 23:15:58 -0500

    finish portfolio croquis and ready to build

--- Projects/portfolio00/components/RecentProjects.tsx
+++ Projects/portfolio00/components/RecentProjects.tsx
@@ -10,7 +10,7 @@ export const RecentProjects = () => {
                 A Small selection of {''}
                 <span className='text-red'>recent projects</span>
             </h1>
-            <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 ga´p-y-8 mt-10'>
+            <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10'>
                 {projects.map(({ id, title, des, img, iconLists, link }) => (
                     <div key={id} className=' sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]'>
                         <PinContainer title={link} href={link}>
